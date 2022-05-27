f1915686c5543010090ab8ea70857052f051a3d7
RaptorJezuz15 <105820613+RaptorJezuz15@users.noreply.github.com>
2022-05-27 17:42:33 -0400

    Adding java backend, complete

---
+++ mechp2/src/main/java/Repo/MechRepo.java
@@ -0,0 +1,8 @@
+package Repo;
+
+import java.util.List;
+import Models.Mech;
+
+public interface MechRepo {
+    public List<Mech> getAllMechs();
+}
---
+++ mechp2/src/main/java/Service/MechService.java
@@ -0,0 +1,8 @@
+package Service;
+
+import java.util.List;
+import Models.Mech;
+
+public interface MechService {
+    public List<Mech> getAllMechs();
+}
